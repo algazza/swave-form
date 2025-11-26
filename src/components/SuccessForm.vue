32a24858c7c0dffdc7793f079541f4836b43d57f
algazza <fadhilaswadira72564@gmail.com>
2025-11-26 20:10:34 +0700

    📱ui: fix responsive

--- src/components/SuccessForm.vue
+++ src/components/SuccessForm.vue
@@ -3,11 +3,11 @@
     class="flex flex-col md:flex-row gap-8 md:gap-16 justify-center items-center mt-6"
   >
     <div class="py-2 px-6 shadow-[8px_8px_0px_2px_#710014] border border-primary text-center">
-      <h1 class="text-4xl font-ramabhadra">
-        <span class="font-imperial mr-2 text-7xl">Y</span>OU HAVE GOOD TASTE
+      <h1 class="text-lg md:text-4xl font-ramabhadra">
+        <span class="font-imperial mr-2 text-4xl md:text-7xl">Y</span>OU HAVE GOOD TASTE
       </h1>
-      <h1 class="text-4xl font-ramabhadra">
-        <span class="font-imperial mr-2 text-7xl">T</span>HANKYOU FOR YOUR ORDER
+      <h1 class="text-lg md:text-4xl font-ramabhadra">
+        <span class="font-imperial mr-2 text-4xl md:text-7xl">T</span>HANKYOU FOR YOUR ORDER
       </h1>
     </div>
   </section>
