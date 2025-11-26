d974cbe86055d59be3a3ba0ce9147c9f89be6e40
algazza <fadhilaswadira72564@gmail.com>
2025-11-26 17:15:45 +0700

    📝 docs: change logo

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import { logo } from './lib/constanta';
 
 <template>
   <main
-    class="mx-2 my-4 md:w-[780px] md:mx-auto sm:px-4 sm:p-4 sm:my-0"
+    class="mx-6 my-4 md:w-[780px] md:mx-auto sm:px-4 sm:p-4 sm:my-0"
   >
     <img :src="logo" alt="swave logo" class="mx-auto my-4 px-8 sm:max-w-[500px]">
 
